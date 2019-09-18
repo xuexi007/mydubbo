d93549d4619db50b11b1fff5ba3a58ba224944fa
sunny <test@qq.com>
2019-09-18 16:24:32 +0800

    首次提交mydubbo项目

---
+++ myConsumer/src/main/java/com/offcn/ConsumerAppStart.java
@@ -0,0 +1,12 @@
+package com.offcn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerAppStart {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerAppStart.class,args);
+    }
+}
---
+++ myInterface/src/main/java/com/offcn/service/HelloService.java
@@ -0,0 +1,6 @@
+package com.offcn.service;
+
+public interface HelloService {
+
+    public String sayHello(String name);
+}
